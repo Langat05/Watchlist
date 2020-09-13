1eb454b51cc0819427bb93a59609adcf682897fc
langat justus <justuslangat78@gmail.com>
2020-09-13 10:56:44 +0300

    initialized sqlelchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,26 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+
 
 bootstrap = Bootstrap()
+db = SQLAlchemy()
 
 def create_app(config_name):
 
     app = Flask(__name__)
 
     # Creating the app configurations
+
     app.config.from_object(config_options[config_name])
+    config_options[config_name].init_app(app)
 
     # Initializing flask extensions
     bootstrap.init_app(app)
+    db.init_app(app)
 
-    # Will add the views and forms
+
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
@@ -22,5 +28,8 @@ def create_app(config_name):
     # setting config
     from .requests import configure_request
     configure_request(app)
+
+
+
 
     return app
